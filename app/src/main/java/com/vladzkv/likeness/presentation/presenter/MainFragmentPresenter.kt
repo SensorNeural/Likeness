a453df5e271b8cd1469b0a654395867642e7ff4e
Vladislav Slavetny <55889260+slavetny@users.noreply.github.com>
2020-03-20 22:10:50 +0200

    create gallery view function

--- app/src/main/java/com/vladzkv/likeness/presentation/presenter/MainFragmentPresenter.kt
+++ app/src/main/java/com/vladzkv/likeness/presentation/presenter/MainFragmentPresenter.kt
@@ -7,9 +7,16 @@ import moxy.MvpPresenter
 /**
  * Created by kirill on 20.03.2020.
  */
+
 @InjectViewState
 class MainFragmentPresenter : MvpPresenter<MainView>() {
+
     override fun onFirstViewAttach() {
         super.onFirstViewAttach()
+
+        setupGalleryView()
+    }
+
+    private fun setupGalleryView() {
     }
 }
