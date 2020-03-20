2cecc4b4920a3f55cf652081d1fd7ea6d8da3473
Kirill <ueurhfyh1@gmail.com>
2020-03-20 16:32:47 +0300

    creating packages

---
+++ app/src/main/java/com/vladzkv/likeness/domain/extensions/extensions.kt
@@ -0,0 +1,2 @@
+package com.vladzkv.likeness.domain.extensions
+
---
+++ app/src/main/java/com/vladzkv/likeness/presentation/ui/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.vladzkv.likeness.presentation.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import com.vladzkv.likeness.R
+
+class MainActivity : AppCompatActivity(R.layout.activity_main)
