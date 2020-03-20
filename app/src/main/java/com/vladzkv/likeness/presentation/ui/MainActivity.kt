2a07da10b782dcd2b7d9e835dd220c443c946529
Kirill <ueurhfyh1@gmail.com>
2020-03-20 23:00:58 +0300

    add onClickListener on fab

--- app/src/main/java/com/vladzkv/likeness/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/vladzkv/likeness/presentation/ui/MainActivity.kt
@@ -2,6 +2,7 @@ package com.vladzkv.likeness.presentation.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 import com.vladzkv.likeness.R
@@ -9,12 +10,15 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        navController = findNavController(R.id.nav_host_fragment)
         NavigationUI.setupWithNavController(
             bottomAppBar,
-            findNavController(R.id.nav_host_fragment)
+            navController
         )
+        fab.setOnClickListener { navController.navigate(R.id.action_mainFragment_to_editPhotoFragment) }
     }
 }
--- app/src/main/java/com/vladzkv/likeness/presentation/ui/fragment/main/MainFragment.kt
+++ app/src/main/java/com/vladzkv/likeness/presentation/ui/fragment/main/MainFragment.kt
@@ -16,9 +16,5 @@ class MainFragment : MvpAppCompatFragment(), MainView {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false)
-    }
-
+    ): View? = inflater.inflate(R.layout.fragment_main, container, false)
 }
