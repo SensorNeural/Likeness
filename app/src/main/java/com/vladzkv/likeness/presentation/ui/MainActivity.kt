db6b8d8daa7504d55cf5d5ef7648be5c621bcc14
Kirill <ueurhfyh1@gmail.com>
2020-03-20 21:19:44 +0300

    -_-

--- app/src/main/java/com/vladzkv/likeness/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/vladzkv/likeness/presentation/ui/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.vladzkv.likeness.presentation.ui
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.vladzkv.likeness.R
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
 }
